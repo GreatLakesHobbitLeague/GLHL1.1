042dc0d1ee2c0e4a78ede11c13871c8394e41256
Todd Skinner <tmskinner01@gmail.com>
2024-03-29 11:12:06 -0400

    added navbar

--- .nuxt/dist/server/server.mjs
+++ .nuxt/dist/server/server.mjs
@@ -1,1 +1,1 @@
-export { default } from "file:///C:/Users/whata/Documents/GLHL/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
+export { default } from "file:///C:/Users/whata/Documents/Projects/GLHL/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
