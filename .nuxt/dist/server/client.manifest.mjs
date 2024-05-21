3182a08d06bd9bcb8afcf437b6bfe079cb3f68f3
Todd Skinner <tmskinner01@gmail.com>
2024-05-20 21:50:25 -0400

    created new branch

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,609 @@
-export { default } from "file:///C:/Users/whata/OneDrive/Documents/GLHL/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
+export default {
+  "_BImCA5dO.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BImCA5dO.js",
+    "name": "browser",
+    "isDynamicEntry": true
+  },
+  "_D5v2Upum.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "D5v2Upum.js",
+    "name": "index",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "dynamicImports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_DQ5Rdo0Q.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DQ5Rdo0Q.js",
+    "name": "HoG",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "assets": [
+      "Gimli_1.BQnHrWd3.png",
+      "HoG.Jxp1R1Ee.png"
+    ]
+  },
+  "Gimli_1.BQnHrWd3.png": {
+    "file": "Gimli_1.BQnHrWd3.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "HoG.Jxp1R1Ee.png": {
+    "file": "HoG.Jxp1R1Ee.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "_MwMJ1d3w.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "MwMJ1d3w.js",
+    "name": "vue.f36acd1f",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "assets/fonts/Ringbearer.TTF": {
+    "prefetch": true,
+    "file": "Ringbearer.CTp9MBLD.TTF",
+    "src": "assets/fonts/Ringbearer.TTF"
+  },
+  "layouts/default.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BusW9Q9r.js",
+    "name": "default",
+    "src": "layouts/default.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/.cache/nuxt-google-fonts/fonts/Cinzel-400-1.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Cinzel-400-1.bAKzRVOf.woff2",
+    "src": "node_modules/.cache/nuxt-google-fonts/fonts/Cinzel-400-1.woff2"
+  },
+  "node_modules/.cache/nuxt-google-fonts/fonts/Cinzel-400-2.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Cinzel-400-2.DGLLPQbV.woff2",
+    "src": "node_modules/.cache/nuxt-google-fonts/fonts/Cinzel-400-2.woff2"
+  },
+  "node_modules/.cache/nuxt-google-fonts/fonts/Uncial_Antiqua-400-3.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Uncial_Antiqua-400-3.DPalS0aZ.woff2",
+    "src": "node_modules/.cache/nuxt-google-fonts/fonts/Uncial_Antiqua-400-3.woff2"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DHmyyUa7.js",
+    "name": "error-404",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_MwMJ1d3w.js"
+    ],
+    "css": []
+  },
+  "error-404.C9qesszH.css": {
+    "file": "error-404.C9qesszH.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DQknJHSi.js",
+    "name": "error-500",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_MwMJ1d3w.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "error-500.yp4PmXS5.css": {
+    "file": "error-500.yp4PmXS5.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "9rS3P3u6.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "isDynamicEntry": true,
+    "dynamicImports": [
+      "_BImCA5dO.js",
+      "layouts/default.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "css": [
+      "entry.HVCHlPuO.css"
+    ],
+    "assets": [
+      "glhl_logo.DZokxolE.png",
+      "anduril.CvqAiO2Y.png",
+      "discord_icon.uUpzBD1n.png",
+      "facebook_icon.CSf-hILG.png"
+    ],
+    "_globalCSS": true
+  },
+  "entry.HVCHlPuO.css": {
+    "file": "entry.HVCHlPuO.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "glhl_logo.DZokxolE.png": {
+    "file": "glhl_logo.DZokxolE.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "anduril.CvqAiO2Y.png": {
+    "file": "anduril.CvqAiO2Y.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "discord_icon.uUpzBD1n.png": {
+    "file": "discord_icon.uUpzBD1n.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "facebook_icon.CSf-hILG.png": {
+    "file": "facebook_icon.CSf-hILG.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "pages/event.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CP_kb-Qd.js",
+    "name": "event",
+    "src": "pages/event.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_D5v2Upum.js",
+      "_DQ5Rdo0Q.js"
+    ],
+    "css": [
+      "event.3iKIV0Aw.css"
+    ],
+    "assets": [
+      "glhl_banner.BthS7pyf.png",
+      "glhl_tactician_gold.D9cwcKj1.png",
+      "glhl_tactician_silver.B_xcZEx4.png",
+      "glhl_tactician_bronze.DAtp34Nf.png",
+      "glhl_artisan_gold.CTQTbx-R.png",
+      "glhl_artisan_silver.DLnkYqmM.png",
+      "glhl_artisan_bronze.5i3M_MTZ.png",
+      "Sting.BjUvYMwC.png",
+      "white_hand.DjE8_Zn8.png"
+    ]
+  },
+  "event.3iKIV0Aw.css": {
+    "file": "event.3iKIV0Aw.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "glhl_banner.BthS7pyf.png": {
+    "file": "glhl_banner.BthS7pyf.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "glhl_tactician_gold.D9cwcKj1.png": {
+    "file": "glhl_tactician_gold.D9cwcKj1.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "glhl_tactician_silver.B_xcZEx4.png": {
+    "file": "glhl_tactician_silver.B_xcZEx4.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "glhl_tactician_bronze.DAtp34Nf.png": {
+    "file": "glhl_tactician_bronze.DAtp34Nf.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "glhl_artisan_gold.CTQTbx-R.png": {
+    "file": "glhl_artisan_gold.CTQTbx-R.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "glhl_artisan_silver.DLnkYqmM.png": {
+    "file": "glhl_artisan_silver.DLnkYqmM.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "glhl_artisan_bronze.5i3M_MTZ.png": {
+    "file": "glhl_artisan_bronze.5i3M_MTZ.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "Sting.BjUvYMwC.png": {
+    "file": "Sting.BjUvYMwC.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "white_hand.DjE8_Zn8.png": {
+    "file": "white_hand.DjE8_Zn8.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "pages/events.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BZI6IJxN.js",
+    "name": "events",
+    "src": "pages/events.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "29wSgIlc.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DQ5Rdo0Q.js",
+      "_D5v2Upum.js"
+    ],
+    "css": [],
+    "assets": [
+      "spark_overlay.B5gSAS5c.webm",
+      "Anduril.DUAGs8rk.png",
+      "Uruk_1.B-Jzy9Ne.png",
+      "bhr_logo.KtENkjAX.webp",
+      "paragon_logo.CvxuFWa6.png",
+      "glhl_gold.vjlmGABa.webp",
+      "red_leather.Dlqdjxyb.webp"
+    ]
+  },
+  "index.Byz1La9X.css": {
+    "file": "index.Byz1La9X.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "spark_overlay.B5gSAS5c.webm": {
+    "file": "spark_overlay.B5gSAS5c.webm",
+    "resourceType": "video",
+    "prefetch": true
+  },
+  "Anduril.DUAGs8rk.png": {
+    "file": "Anduril.DUAGs8rk.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "Uruk_1.B-Jzy9Ne.png": {
+    "file": "Uruk_1.B-Jzy9Ne.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "bhr_logo.KtENkjAX.webp": {
+    "file": "bhr_logo.KtENkjAX.webp",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp"
+  },
+  "paragon_logo.CvxuFWa6.png": {
+    "file": "paragon_logo.CvxuFWa6.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "glhl_gold.vjlmGABa.webp": {
+    "file": "glhl_gold.vjlmGABa.webp",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp"
+  },
+  "red_leather.Dlqdjxyb.webp": {
+    "file": "red_leather.Dlqdjxyb.webp",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp"
+  },
+  "pages/rankings.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "By7KM8IF.js",
+    "name": "rankings",
+    "src": "pages/rankings.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_D5v2Upum.js"
+    ]
+  },
+  "public/images/anduril.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "anduril.CvqAiO2Y.png",
+    "src": "public/images/anduril.png"
+  },
+  "public/images/cinematics/black_guard_desktop.webp": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp",
+    "file": "black_guard_desktop.DVUGuK_9.webp",
+    "src": "public/images/cinematics/black_guard_desktop.webp"
+  },
+  "public/images/cinematics/hero_desktop.jpg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg",
+    "file": "hero_desktop.C_XU-Jjg.jpg",
+    "src": "public/images/cinematics/hero_desktop.jpg"
+  },
+  "public/images/cinematics/hero_desktop.webp": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp",
+    "file": "hero_desktop.CSxaxEmv.webp",
+    "src": "public/images/cinematics/hero_desktop.webp"
+  },
+  "public/images/cinematics/hero_mobile.webp": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp",
+    "file": "hero_mobile.CF18yCSy.webp",
+    "src": "public/images/cinematics/hero_mobile.webp"
+  },
+  "public/images/cinematics/spark_overlay.webm": {
+    "resourceType": "video",
+    "prefetch": true,
+    "file": "spark_overlay.B5gSAS5c.webm",
+    "src": "public/images/cinematics/spark_overlay.webm"
+  },
+  "public/images/cinematics/woses_desktop.webp": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp",
+    "file": "woses_desktop.D_xW_mwm.webp",
+    "src": "public/images/cinematics/woses_desktop.webp"
+  },
+  "public/images/glhl_banner.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "glhl_banner.BthS7pyf.png",
+    "src": "public/images/glhl_banner.png"
+  },
+  "public/images/glhl_banner_mobile.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "glhl_banner_mobile.BuncwOF0.png",
+    "src": "public/images/glhl_banner_mobile.png"
+  },
+  "public/images/glhl_logo.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "glhl_logo.DZokxolE.png",
+    "src": "public/images/glhl_logo.png"
+  },
+  "public/images/icons/discord_icon.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "discord_icon.uUpzBD1n.png",
+    "src": "public/images/icons/discord_icon.png"
+  },
+  "public/images/icons/facebook_icon.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "facebook_icon.CSf-hILG.png",
+    "src": "public/images/icons/facebook_icon.png"
+  },
+  "public/images/map-background.webp": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp",
+    "file": "map-background.-GtneC3q.webp",
+    "src": "public/images/map-background.webp"
+  },
+  "public/images/mesbg-silhouettes/Anduril.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "Anduril.DUAGs8rk.png",
+    "src": "public/images/mesbg-silhouettes/Anduril.png"
+  },
+  "public/images/mesbg-silhouettes/Gimli_1.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "Gimli_1.BQnHrWd3.png",
+    "src": "public/images/mesbg-silhouettes/Gimli_1.png"
+  },
+  "public/images/mesbg-silhouettes/HoG.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "HoG.Jxp1R1Ee.png",
+    "src": "public/images/mesbg-silhouettes/HoG.png"
+  },
+  "public/images/mesbg-silhouettes/Sting.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "Sting.BjUvYMwC.png",
+    "src": "public/images/mesbg-silhouettes/Sting.png"
+  },
+  "public/images/mesbg-silhouettes/Uruk_1.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "Uruk_1.B-Jzy9Ne.png",
+    "src": "public/images/mesbg-silhouettes/Uruk_1.png"
+  },
+  "public/images/mesbg-silhouettes/white_hand.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "white_hand.DjE8_Zn8.png",
+    "src": "public/images/mesbg-silhouettes/white_hand.png"
+  },
+  "public/images/rankings/glhl_artisan_bronze.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "glhl_artisan_bronze.5i3M_MTZ.png",
+    "src": "public/images/rankings/glhl_artisan_bronze.png"
+  },
+  "public/images/rankings/glhl_artisan_gold.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "glhl_artisan_gold.CTQTbx-R.png",
+    "src": "public/images/rankings/glhl_artisan_gold.png"
+  },
+  "public/images/rankings/glhl_artisan_silver.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "glhl_artisan_silver.DLnkYqmM.png",
+    "src": "public/images/rankings/glhl_artisan_silver.png"
+  },
+  "public/images/rankings/glhl_tactician_bronze.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "glhl_tactician_bronze.DAtp34Nf.png",
+    "src": "public/images/rankings/glhl_tactician_bronze.png"
+  },
+  "public/images/rankings/glhl_tactician_gold.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "glhl_tactician_gold.D9cwcKj1.png",
+    "src": "public/images/rankings/glhl_tactician_gold.png"
+  },
+  "public/images/rankings/glhl_tactician_silver.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "glhl_tactician_silver.B_xcZEx4.png",
+    "src": "public/images/rankings/glhl_tactician_silver.png"
+  },
+  "public/images/sponsors/bhr_logo.webp": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp",
+    "file": "bhr_logo.KtENkjAX.webp",
+    "src": "public/images/sponsors/bhr_logo.webp"
+  },
+  "public/images/sponsors/paragon_logo.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "paragon_logo.CvxuFWa6.png",
+    "src": "public/images/sponsors/paragon_logo.png"
+  },
+  "public/images/textures/black_iron.webp": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp",
+    "file": "black_iron.BJfDy_iy.webp",
+    "src": "public/images/textures/black_iron.webp"
+  },
+  "public/images/textures/glhl_bronze.webp": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp",
+    "file": "glhl_bronze.CVuacjyv.webp",
+    "src": "public/images/textures/glhl_bronze.webp"
+  },
+  "public/images/textures/glhl_gold.webp": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp",
+    "file": "glhl_gold.vjlmGABa.webp",
+    "src": "public/images/textures/glhl_gold.webp"
+  },
+  "public/images/textures/glhl_silver.webp": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp",
+    "file": "glhl_silver.CFPavnjq.webp",
+    "src": "public/images/textures/glhl_silver.webp"
+  },
+  "public/images/textures/red_leather.webp": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/webp",
+    "file": "red_leather.Dlqdjxyb.webp",
+    "src": "public/images/textures/red_leather.webp"
+  }
+}
