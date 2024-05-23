9d990e1876a6b6c0a634c7dd16f2bb1985a456bd
Todd Skinner <tmskinner01@gmail.com>
2024-05-22 21:23:00 -0400

    updated rankings font wieght to reflect whether qualified or not

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,18 +1,1 @@
-export default {
-  "@vite/client": {
-    "prefetch": true,
-    "isEntry": true,
-    "file": "@vite/client",
-    "css": [],
-    "module": true,
-    "resourceType": "script"
-  },
-  "C:/Users/whata/OneDrive/Documents/GLHL/node_modules/nuxt/dist/app/entry.js": {
-    "resourceType": "script",
-    "module": true,
-    "prefetch": true,
-    "preload": true,
-    "isEntry": true,
-    "file": "C:/Users/whata/OneDrive/Documents/GLHL/node_modules/nuxt/dist/app/entry.js"
-  }
-}
+export { default } from "file:///C:/Users/whata/OneDrive/Documents/GLHL/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/dist/server/server.mjs
+++ .nuxt/dist/server/server.mjs
@@ -1,6 +1,1 @@
-const entrySpa = () => {
-};
-export {
-  entrySpa as default
-};
-//# sourceMappingURL=server.mjs.map
+export { default } from "file:///C:/Users/whata/OneDrive/Documents/GLHL/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
